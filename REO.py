3e2f2fd27f2148cd171434d3d9e39f71dd270d96
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-11-02 12:04:22 +0100

    Initial

---
+++ REO.py
@@ -0,0 +1,8 @@
+name = input("Enter your name :")
+age = int(input("Enter your age :"))
+
+if age < 16 :
+ print("You are not eligible to write this exam")
+
+else :
+ print("You are eligible to write the exams")
